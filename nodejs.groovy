bcdfc0416bf3d459564d55854bb0efe4a9163e6b
pooja201095 <poojachavan153@gmail.com>
2022-09-11 17:40:21 +0530

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,9 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/pooja201095/Node-Docker-Demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-        }
+        git('https://github.com/pooja201095/Node-Docker-Demo.git')
     }
     triggers {
         scm('H/5 * * * *')
